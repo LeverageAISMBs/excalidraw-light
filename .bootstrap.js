2bc60fc4f2666077359c23a77a0d79375e97e063
Vibesdk <vibesdk-bot@cloudflare.com>
2025-11-26 15:41:05 +0000

    chore: Update bootstrap script with latest commands

--- .bootstrap.js
+++ .bootstrap.js
@@ -81,7 +81,8 @@ function runSetupCommands() {
     "bun add roughjs@^4.0.6",
     "bun add react-colorful@^5.6.1",
     "bun install @use-gesture/react",
-    "bun install uuid"
+    "bun install uuid",
+    "bun add react-colorful"
 ];
     
     if (commands.length === 0) {
